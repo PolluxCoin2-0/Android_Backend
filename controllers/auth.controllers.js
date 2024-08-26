50f6d3533ccf5927ac5c09731fd4d8a06fac86b5
gshubham804 <gshubham804@gmail.com>
2024-08-26 17:57:09 +0530

    updateToken api done

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -34,3 +34,36 @@ export const register = async (req, res, next) => {
     }
   };
   
+
+  export const updateToken = async (req, res, next) => {
+    const { walletAddress, token } = req.body;
+  
+    try {
+      // Check if a user with the given walletAddress exists
+      const existingUser = await User.findOne({ walletAddress: walletAddress });
+  
+      if (!existingUser) {
+        return res.status(404).json({
+          status: "error",
+          message: "Wallet Address not found. Please register first.",
+        });
+      } else {
+        // If user exists, update the token
+        existingUser.token = token;
+        await existingUser.save();
+  
+        return res.status(200).json({
+          status: "success",
+          message: "Token updated successfully.",
+        });
+      }
+    } catch (error) {
+      // Handle any errors that occurred during the token update process
+      console.error("Error during token update:", error);
+      return res.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+      });
+    }
+  };
+  
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import {register} from "../controllers/auth.controllers.js";
+import {register, updateToken} from "../controllers/auth.controllers.js";
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/updateToken", updateToken);
 
 export default router;
